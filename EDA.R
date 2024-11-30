168a51ca9699926388d1ac93cb8efebc24ed110a
Federico Salinas Samaniego <salinasfederico773@gmail.com>
2024-11-29 20:55:51 -0600

    Initial EDA and data treatmet

--- EDA.R
+++ EDA.R
@@ -1,6 +1,18 @@
-datos_burnout <- read.csv("./data/train.csv")
-datos_burnout
+library(dplyr)
 
+
+datos_burnout <- read.csv("./data/train.csv")
+
+# Removemos la columna Employee.ID
+datos_burnout <- datos_burnout[, -which(names(datos_burnout) == "Employee.ID")]
+head(datos_burnout)
+
+# Revisamos el numero de NAs por cada variable
 colSums(is.na(datos_burnout))
 
-library(misty)
+# Removemos filas con Burn.Rate con NA
+datos_burnout <- datos_burnout[-which(is.na(datos_burnout$Burn.Rate)), ]
+colSums(is.na(datos_burnout))
+
+#lm_burnout <- lm(Burn.Rate ~ . - Burn.Rate - Employee.ID - Date.of.Joining, data = datos_burnout)
+#summary(lm_burnout)
