60f706f657df2bc0881002951508ec53ed29a7e3
Federico Salinas Samaniego <salinasfederico773@gmail.com>
2024-12-02 09:23:14 -0600

    Removed unused R file

--- Misssing Data.r
+++
@@ -1,6 +0,0 @@
-# Se remueven los datos faltantes de la variable predictora, burn Rate
-train_data <- train_data[!is.na(train_data$`Burn Rate`), ]
-colSums(is.na(train_data))
-
-
-# Revisamos patrones de los datos faltantes de las variables Mental Fatigue Score y Resource Allocation
